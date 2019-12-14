0562ca4f1c2acc3916b44c8cfae2ae386e3994d3
Sarah Page <sarah.page87@gmail.com>
2019-12-14 22:50:15 +0000

    Colors

--- reactjs/src/App.tsx
+++ reactjs/src/App.tsx
@@ -1,29 +1,28 @@
-import React, { Component } from 'react';
-import Dashboard from './Components/Dashboard/Dashboard';
-import {createGlobalStyle} from 'styled-components';
-import Header from './Components/Header/Header';
+import React, { Component } from "react";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import { createGlobalStyle } from "styled-components";
+import Header from "./Components/Header/Header";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300&display=swap');
 
     body {
         background-color:#000;
-        color: #fff;
+        color: #e1e1e1;
         font-family: 'Open Sans Condensed', sans-serif;
     }
-`
+`;
 
 class App extends Component {
-
-    render() {
-        return (
-            <React.Fragment>
-                <Header />
-                <GlobalStyle />
-                <Dashboard />
-            </React.Fragment>
-        )
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <GlobalStyle />
+        <Dashboard />
+      </React.Fragment>
+    );
   }
-  
-  export default App;
+}
+
+export default App;
--- reactjs/src/Components/Tile/Tile.tsx
+++ reactjs/src/Components/Tile/Tile.tsx
@@ -1,25 +1,28 @@
-import React, { Component } from 'react';
-import { TileSize } from '../Tile/TileSize';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import { TileSize } from "../Tile/TileSize";
+import styled from "styled-components";
 
 export interface TileProps {
-  tileSize : TileSize;
+  tileSize: TileSize;
 }
 
 const Wrapper = styled.section<TileProps>`
   grid-column-start: 1;
-  grid-column-end: ${props => props.tileSize == TileSize.Large ? 4 : props.tileSize == TileSize.Medium ? 3 : 2};
-  background-color: #666;
-`
+  grid-column-end: ${props =>
+    props.tileSize == TileSize.Large
+      ? 4
+      : props.tileSize == TileSize.Medium
+      ? 3
+      : 2};
+  background-color: #1e1e1e;
+`;
 
 class Tile extends Component<TileProps> {
-    render() {
-      return (
-        <Wrapper tileSize={this.props.tileSize}>
-            {this.props.children}
-        </Wrapper>
-      )
-    }
+  render() {
+    return (
+      <Wrapper tileSize={this.props.tileSize}>{this.props.children}</Wrapper>
+    );
   }
-  
-  export default Tile;
+}
+
+export default Tile;
